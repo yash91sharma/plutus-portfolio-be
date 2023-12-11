efe65232c8993889be0f04f46792e0489694b223
Yash <yash91sharma@gmail.com>
2023-12-11 04:00:25 +0000

    change the sample output for testing with FE

--- src/getPortfolioSummary.ts
+++ src/getPortfolioSummary.ts
@@ -2,14 +2,23 @@ import express, { Request, Response } from "express";
 
 export const getPortfolioSummary = express.Router();
 
+export interface LineGraphData {
+  date: string;
+  value: number[];
+}
+
 export interface getPortfolioSummaryResponse {
-  comment: string;
-  data: number;
+  portfolioLabels: string[];
+  portfolioData: LineGraphData[];
 }
 
 const sampleResponse: getPortfolioSummaryResponse = {
-  comment: "hello",
-  data: 10009,
+  portfolioLabels: ["Portfolio 1", "QQQ", "S&P500"],
+  portfolioData: [
+    { date: "1 Jan 2023", value: [100, 200, 300] },
+    { date: "2 Jan 2023", value: [105, 205, 305] },
+    { date: "3 Jan 2023", value: [90, 200, 350] },
+  ],
 };
 
 getPortfolioSummary.get(
